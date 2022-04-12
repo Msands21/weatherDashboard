19e8f91b23b37936b570845519a5b2e28d93831d
Mike Sands <mikesands94@gmail.com>
2022-04-11 21:20:09 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # weatherDashboard
 weatherDashboard Server API hw
+## Project Decscription
+In this project, javascript functionality was implemented to grab data from an api from openweathermap.org. This gives the ability to display current weather data from any city in the world upon request. The five day forecast is also displayed. Local storage was also used to store previous searches. 
+## Project Location
+Repo Link: https://github.com/Msands21/weatherDashboard
+
+Live Link: https://msands21.github.io/weatherDashboard/
+
+### Contact Info
+mikesands94@gmail.com
+
+#### Project Example
+![Preview](./assets/Screen%20Shot%202022-04-11%20at%209.17.09%20PM.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ const apiKey = "7ed2fa3c55bb5a9d8cb79cb7ce9d1e4b";
 
 // DOM element references
 // #search form, #search-input, #today, #forecast, #history
-let searchForm = document.querySelector("#search-form");
-let searchInput = document.querySelector("#search-input");
-let todayBox = document.querySelector("#today-box");
-let forecastBox = document.querySelector("#forecast-box");
-let searchDisplayBox = document.querySelector("#searchDisplay-box");
+let searchBtn = document.querySelector("#search-btn");
+let searchInput = document.querySelector("#city-input");
+let currentForecast = document.querySelector(".currentForecast");
+let fiveDay = document.querySelector(".fiveDay");
+let recentSearches = document.querySelector(".cityList");
 
 // Add Timezone for Day.js
 dayjs.extend(window.dayjs_plugin_utc);
@@ -17,10 +17,11 @@ dayjs.extend(window.dayjs_plugin_timezone);
 
 
 
-var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`;
-console.log(weatherApiUrl);
+var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?appid=7ed2fa3c55bb5a9d8cb79cb7ce9d1e4b&q=Orlando`;
+//set up lat and long variable for api key and put in url $`{}`
 
 function weatherData(weatherApi) {
+    
     fetch(weatherApi)
     .then(function(res) {
         res.json()
@@ -30,4 +31,21 @@ function weatherData(weatherApi) {
     })
 }
 
+
 weatherData(weatherApiUrl);
+
+
+// // Function for current weather --> set up temp/humidity/wind etc variables
+
+
+// // Day.js to go in current forecast function
+// cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
+// // For loop after fetch data (loop wihthin a function)
+
+//     for(let i=1; i < 6; i++){
+
+//     }
+// // history array for search history (loop through it)
+
+// // Function for weather forecast (5-day) --> set up temp/humidity/wind etc variables
+// cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
